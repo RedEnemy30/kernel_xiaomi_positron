82df9e484629dc64a43e4969408540feb4bb0e41
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-19 19:17:42 -0800

    cpumask: Add cpumasks for big, LITTLE, and prime CPU clusters
    Add cpu_lp_mask, cpu_perf_mask, and cpu_prime_mask to represent the CPUs
    that belong to each cluster in a tri-cluster, heterogeneous system.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -56,6 +56,9 @@ extern unsigned int nr_cpu_ids;
  *     cpu_online_mask  - has bit 'cpu' set iff cpu available to scheduler
  *     cpu_active_mask  - has bit 'cpu' set iff cpu available to migration
  *     cpu_isolated_mask- has bit 'cpu' set iff cpu isolated
+ *     cpu_lp_mask      - has bit 'cpu' set iff cpu is part of little cluster
+ *     cpu_perf_mask    - has bit 'cpu' set iff cpu is part of big cluster
+ *     cpu_prime_mask   - has bit 'cpu' set iff cpu is part of prime cluster
  *
  *  If !CONFIG_HOTPLUG_CPU, present == possible, and active == online.
  *
@@ -103,6 +106,9 @@ extern struct cpumask __cpu_isolated_mask;
 #endif
 
 extern atomic_t __num_online_cpus;
+extern const struct cpumask *const cpu_lp_mask;
+extern const struct cpumask *const cpu_perf_mask;
+extern const struct cpumask *const cpu_prime_mask;
 
 #if NR_CPUS > 1
 /**
