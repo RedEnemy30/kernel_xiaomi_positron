3602442af96e67cf9feb4a74c8315d79506eabc9
Dark-Matter7232 <me@const.eu.org>
2023-10-10 16:56:07 +0530

    kernel/sched: walt: do not build core_ctl
    Change-Id: Id2dd61163927d12ba2ba3ff5bf1612f2b4f0adef
    Signed-off-by: Dark-Matter7232 <me@const.eu.org>

--- include/linux/sched/core_ctl.h
+++ include/linux/sched/core_ctl.h
@@ -20,16 +20,11 @@ struct core_ctl_notif_data {
 
 struct notifier_block;
 
-#ifdef CONFIG_SCHED_WALT
-extern int core_ctl_set_boost(bool boost);
-extern void core_ctl_notifier_register(struct notifier_block *n);
-extern void core_ctl_notifier_unregister(struct notifier_block *n);
-#else
+static inline void core_ctl_check(u64 wallclock) {}
 static inline int core_ctl_set_boost(bool boost)
 {
 	return 0;
 }
 static inline void core_ctl_notifier_register(struct notifier_block *n) {}
 static inline void core_ctl_notifier_unregister(struct notifier_block *n) {}
-#endif
 #endif
